a3c79c6a163a424d0a8198142f05021d4c196552
Gabriel <tandohtakyie@hotmail.com>
2019-09-11 10:54:13 +0200

    final deliver of pictureQuiz

---
+++ Projects/app/src/main/java/com/example/picturequiz/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.picturequiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnConfirm.setOnClickListener{
+            checkAnswer();
+        }
+    }
+
+    /**
+     *  Check if the submitted answer is correct
+     */
+    private fun checkAnswer() {
+        val answer = etAnswer.text.toString()
+
+        // When the answer equals "giraffe" then display a correct
+        // message using a toast message
+        // Otherwise display an incorrect message.
+        if (answer == getString(R.string.giraffe)) {
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        }
+    }
+}
